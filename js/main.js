adb7e0cec33600f9ddd341283d6de4797217c6f0
Edex01 <jorasiac@gmail.com>
2020-09-01 18:35:05 +0530

    global variables

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// global variables
+
+var workClicks = 20;
+var breakClicks = 5;
+var workDisplay = document.getElementById("work-display");
+var breakDisplay = document.getElementById("break-display");
+var count = 0;
+var workSession;
+var breakSession;
+
