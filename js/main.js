12c65b7cab33760f2f32d5b0eee5b524c2fe13b6
Edex01 <jorasiac@gmail.com>
2020-09-03 17:11:09 +0530

    increasework function added

--- js/main.js
+++ js/main.js
@@ -8,3 +8,10 @@ var count = 0;
 var workSession;
 var breakSession;
 
+// increase work
+
+var increaseWork = document.getElementById("addWorkButton");
+increaseWork.addEventListener("click", function(){
+	workClicks+= 1;
+	workDisplay.innerHTML = workClicks;
+}, false);
